f16f38abd850eb030fbd5f987b0dcd5ebd9e446f
Qifan Chang <qchang@Qifans-MacBook.local>
2018-10-12 04:09:01 -0400

    finally made home page look like a business card

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,11 @@
-$( document ).ready(function() {
-
+$(document).ready(function() {
+  width_height_ratio_lock();
   // Get started!
-
 });
+
+function width_height_ratio_lock() {
+  var section = $("#my-business-card");
+  var width = section.width() + 80;
+
+  section.css("height", (4 / 7) * width - 80);
+}
